e63abc025d8242614a20873533645d617e50a8b9
Dag <jegisew21@gmail.com>
2024-09-18 00:09:28 +0300

    unit test

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import lists
+import lists.views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', lists.views.home_page, name='home')
+    # path('admin/', admin.site.urls),
 ]
