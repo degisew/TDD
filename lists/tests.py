14602d2d7e8c3ded6bb8e14516ec027854c995c6
degisew <jegisew21@gmail.com>
2024-09-20 23:00:12 +0300

    test for viewa

--- lists/tests.py
+++ lists/tests.py
@@ -4,14 +4,20 @@ from django.test import TestCase
 from lists.views import home_page
 
 
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+def test_root_url_resolves_to_home_page_view():
+    found = resolve('/')
+    assert found.func == home_page
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+# def test_home_page_returns_correct_html():
+#     request = HttpRequest()
+#     response = home_page(request)
+#     self.assertTrue(response.content.startswith(b'<html>'))
+#     self.assertIn(b'<title>To-Do lists</title>', response.content)
+#     self.assertTrue(response.content.endswith(b'</html>'))
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    response = home_page(request)
+    
+    assert response.content.startswith(b'<html>')
+    assert b'<title>To-Do lists</title>' in response.content
+    assert response.content.endswith(b'</html>')
