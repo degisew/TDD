19e91fbe5966451801da16f13ebbe495f5984a46
degisew <jegisew21@gmail.com>
2024-11-14 16:14:34 +0300

    Wrote a test for serializers

--- tests/movies/test_models.py
+++ tests/movies/test_models.py
@@ -1,15 +1,37 @@
+from django.forms import ValidationError
 import pytest
 from model_bakery import baker
-from movies.models import Movie  # Replace `myapp` with the actual app name
+from movies.models import Movie
 
 
 @pytest.mark.django_db
-def test_movie_model():
-    movie = baker.make(Movie)
+def test_movie_creation() -> None:
+    """Test that a Movie instance can be created and has required fields."""
+    movie = baker.make(Movie, year="2023", title="Valid Title")
 
-    assert movie.id is not None 
-
-    assert movie.created_date
-    assert movie.updated_date
-    # Test the __str__ method
+    assert movie.id is not None
+    assert movie.created_date is not None
+    assert movie.updated_date is not None
     assert str(movie) == movie.title
+
+
+@pytest.mark.django_db
+def test_movie_year_max_length_validation() -> None:
+    """Test that the year field enforces max_length=4."""
+    movie = baker.prepare(Movie, year="12345")  # Use prepare to avoid saving
+
+    with pytest.raises(ValidationError) as excinfo:
+        movie.full_clean()
+
+    assert "Ensure this value has at most 4 characters" in str(excinfo.value)
+
+
+@pytest.mark.django_db
+def test_movie_title_required() -> None:
+    """Test that the title field cannot be empty if required."""
+    movie = baker.prepare(Movie, title="")
+
+    with pytest.raises(ValidationError) as excinfo:
+        movie.full_clean()
+
+    assert "This field cannot be blank" in str(excinfo.value)
---
+++ tests/movies/test_serializers.py
@@ -0,0 +1,27 @@
+from movies.serializers import MovieSerializer
+
+
+def test_valid_data_deserialization() -> None:
+    valid_payload = {
+        "title": "Yewendoch Guday",
+        "genre": "comedy",
+        "year": "1999"
+    }
+    serializer = MovieSerializer(data=valid_payload)
+    assert serializer.is_valid()
+    assert serializer.validated_data == valid_payload
+    assert serializer.data == valid_payload
+    assert serializer.errors == {}
+
+
+def test_invalid_data_deserialization() -> None:
+    invalid_payload = {
+        "title": "Fikir Siferd",
+        "genre": "romantic"
+    }
+
+    serializer = MovieSerializer(data=invalid_payload)
+    assert not serializer.is_valid()
+    assert serializer.validated_data == {}
+    assert serializer.data == invalid_payload
+    assert serializer.errors == {"year": ["This field is required."]}
