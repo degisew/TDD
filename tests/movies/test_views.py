1adaf87659e8482fd7a036078bdbfd5ea6788f17
degisew <jegisew21@gmail.com>
2024-11-16 13:42:48 +0300

    test views

---
+++ tests/movies/test_views.py
@@ -0,0 +1,23 @@
+import pytest
+from model_bakery import baker
+from movies.models import Movie
+
+
+@pytest.mark.django_db
+def test_post_method_create_object(client) -> None:
+
+    # Given
+    url = "/api/movies/"
+    payload = baker.prepare(Movie)
+
+    instance_count = Movie.objects.count()
+
+    assert instance_count == 0
+
+    # When
+    response = client.post(url, payload, content_type="application/json")
+
+    # Then
+    assert response.status_code == 201
+
+    assert  instance_count == 1
